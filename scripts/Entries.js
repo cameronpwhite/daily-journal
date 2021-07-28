86b016b68bf39b7a750320aac2a8117bf3620c51
Cameron White <cameronpwhite98@gmail.com>
2021-07-28 10:43:28 -0500

    pushing application to github

---
+++ scripts/Entries.js
@@ -0,0 +1,24 @@
+/*
+    Which function allows this component to get a copy
+    of the data? Import it on the following line of code
+    and then invoke it on the third line of code.
+*/
+import { getJournalEntries } from "./database.js"
+
+
+export const Entries = () => {
+    const entries = getJournalEntries()
+    const allEntriesAsHTML = ""
+
+
+    for (const entry of entries) {
+        allEntriesAsHTML += `
+         <p>  ${entry.entry} </p>
+        `
+    }
+
+
+
+    return allEntriesAsHTML
+}
+
